1cdcedc690b98f953df3dd79bed88bea9c5988bb
Tyler Knapp <knapptyler94@gmail.com>
2017-01-30 19:49:12 -0800

    Create DealerLogic.h

---
+++ DealerLogic.h
@@ -0,0 +1,37 @@
+//Dealer logic
+#include "Class.h"
+#include<iostream>
+#include<vector>
+
+
+char DealerLogic(card up, card down)
+{
+	int total=up.get_value()+down.get_value();
+	bool aces=false; //does the dealer have an ace
+	//checking
+	if(up.get_value==1 || down.get_value==1)
+	{
+		aces==true;
+	}
+	
+	//dealer hits less than 17
+	if(total<17)
+	{
+		return 'h';
+	}
+	//dealer hits on soft 17
+	if(total==17 && aces==true)
+	{
+		return 'h';
+	}
+	//dealer stays on hard 17
+	if(total==17 && aces==false)
+	{
+		return 's';
+	}
+	//dealer stays everywhere else
+	else
+	{
+		return 's';
+	}
+}
