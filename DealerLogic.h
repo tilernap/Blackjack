386660b7c4904db776706d3ef1b07fac3e0ddd74
Tyler Knapp <knapptyler94@gmail.com>
2017-02-01 15:39:30 -0800

    Update DealerLogic.h

--- DealerLogic.h
+++ DealerLogic.h
@@ -1,31 +1,31 @@
 //Dealer logic
-#include "Class.h"
+#include "Classes.h"
 #include<iostream>
 #include<vector>
 
 
 char DealerLogic(card up, card down)
 {
-	int total=up.get_value()+down.get_value();
-	bool aces=false; //does the dealer have an ace
-	//checking
-	if(up.get_value==1 || down.get_value==1)
+	int total = up.get_value() + down.get_value();
+	bool aces = false; //does the dealer have an ace
+					   //checking
+	if (up.get_value == 1 || down.get_value == 1)
 	{
-		aces==true;
+		aces == true;
 	}
-	
+
 	//dealer hits less than 17
-	if(total<17)
+	if (total<17)
 	{
 		return 'h';
 	}
 	//dealer hits on soft 17
-	if(total==17 && aces==true)
+	if (total == 17 && aces == true)
 	{
 		return 'h';
 	}
 	//dealer stays on hard 17
-	if(total==17 && aces==false)
+	if (total == 17 && aces == false)
 	{
 		return 's';
 	}
